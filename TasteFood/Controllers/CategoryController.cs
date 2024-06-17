14d74d9e40d959cf927e553c1b7c75124ab2f138
Selcan Vuruk <Selcanvuruk17@gmail.com>
2024-06-18 00:53:32 +0300

    .

---
+++ TasteFood/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFood.Context;
+using TasteFood.Entities;
+
+namespace TasteFood.Controllers
+{
+    public class CategoryController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult CategoryList()
+        {
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateCategory() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
